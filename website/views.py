0c0e14135a30fc1e3d2dcc2c72ec1fd1d3366d8f
Connor Hamilton <clyhamilton@gmail.com>
2023-04-30 22:17:34 -0400

    Just committing
    Unsure if I committed last Friday. Doing it to be safe

--- website/views.py
+++ website/views.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from flask_login import current_user, login_required
+from flask_login import login_required
 from sqlalchemy import text
 from .models import *
 
-
-
 views = Blueprint('views', __name__)
-
 
 @views.route('/')
 def index():
     return render_template("index.html")
 
+
+@views.route('/home')
+def home():
+    return render_template("design-concept.html")
